048a8ed0a9aa29fd46717dc77c1e367cfd70238d
Vinicius Lorenz <vinicius_lorenz@hotmail.com>
2024-09-06 17:46:53 -0300

    Rewriting text and renaming variables and functions to suit the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,78 @@
 "use client"
 import { useState } from "react";
 
-type Tarefas ={
-  check:boolean;
+type ItensCarrinho ={
+  check: boolean;
   label: string;
 }
 
 const page = ()=>{
   const [checkCounter, setcheckCounter] = useState<number>(0);
-  const [tarefa, setTarefa] = useState<string>("");
-  const [listaTarefas, setlistaTarefas] = useState<Tarefas[]>([]);
-  const handleAdicionar = (tarefa:string) => {
-    if(tarefa.trim() == "") return;
-    setlistaTarefas([...listaTarefas,{check:false,label: tarefa}]);
-    setTarefa("");
+  const [item, setItem] = useState<string>("");
+  const [listaCompras, setlistaCompras] = useState<ItensCarrinho[]>([]);
+  const handleAdicionar = (item:string) => {
+    if(item.trim() == "") return;
+    setlistaCompras([...listaCompras,{check:false,label: item}]);
+    setItem("");
   };
-  const handleRemover = (id: number) =>{
-    const newLista = (listaTarefas.filter((tarefa, index) => index !== id));
+  const handleDeletar = (id: number) =>{
+    const newLista = (listaCompras.filter((item, index) => index !== id));
     setcheckCounter(checkCounter=>0);
-    setlistaTarefas(newLista);
-    newLista.map((tarefa) => {
-      if(tarefa.check == true){
+    setlistaCompras(newLista);
+    newLista.map((item) => {
+      if(item.check == true){
         setcheckCounter(checkCounter=>checkCounter+1);
       }
     });
   }
   const handleCheck = (id: number) =>{
-    const newLista = listaTarefas.map((tarefa, index)=>{
+    const newLista = listaCompras.map((item, index)=>{
       if (index == id){
-        return({...tarefa, check:!tarefa.check});
+        return({...item, check:!item.check});
       }
-      return(tarefa);
+      return(item);
     });
-    setlistaTarefas(newLista);
+    setlistaCompras(newLista);
     setcheckCounter(checkCounter=>0);
-    newLista.map((tarefa) => {
-      if(tarefa.check == true){
+    newLista.map((item) => {
+      if(item.check == true){
         setcheckCounter(checkCounter=>checkCounter+1);
       }
     });
   }
-  const handleLimpar = () => {
-    setlistaTarefas([]);
+  const handleApagarLista = () => {
+    setlistaCompras([]);
     setcheckCounter(0);
   }
-  const handleConcluidos = () => {
-    setlistaTarefas(listaTarefas.filter((tarefa) => tarefa.check != true));
+  const handleApagarCarrinho = () => {
+    setlistaCompras(listaCompras.filter((item) => item.check != true));
     setcheckCounter(0);
   }
 
   return(
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-black text-white">
-      <h3 className="text-3xl">Lista de Tarefas</h3>
+      <h3 className="text-3xl">Lista de Compras</h3>
       <div className="border-solid border-1 border-slate-600 bg-slate-600 rounded-t-md mt-2 p-3 w-96 flex">
-          <input onKeyDown={(e)=>{if(e.key==="Enter"){handleAdicionar(tarefa)}}} className="grow rounded-md p-2 text-black" type="text" placeholder="O que deseja fazer?" value={tarefa} onChange={e => setTarefa(e.target.value)}/>
-          <button className="ml-2 p-2 rounded-md border-solid border-2 hover:bg-blue-400" onClick={e=>handleAdicionar(tarefa)}>Adicionar</button>
+          <input onKeyDown={(e)=>{if(e.key==="Enter"){handleAdicionar(item)}}} className="grow rounded-md p-2 text-black" type="text" placeholder="O que deseja comprar?" value={item} onChange={e => setItem(e.target.value)}/>
+          <button className="ml-2 p-2 rounded-md border-solid border-2 hover:bg-blue-400" onClick={e=>handleAdicionar(item)}>Adicionar</button>
       </div>
       <div className="w-96 grid bg-slate-300 text-black">
-        <p className="mt-2 justify-self-center"> Itens na lista: {listaTarefas.length}</p>
+        <p className="mt-2 justify-self-center"> Itens na lista: {listaCompras.length}</p>
         <ul className="list-disc ml-2 my-2">
-          {listaTarefas.map((tarefa, index)=>
+          {listaCompras.map((item, index)=>
             <li key={index} className="flex items-center mt-1">
-              <input type="checkbox" checked={tarefa.check} className="pt-2 mr-2 w-4 h-4 rounded-md" onChange={e => handleCheck(index)}/>
-              <p className={tarefa.check?"line-through decoration-2":""} onClick={e => handleCheck(index)}>{tarefa.label}</p>
+              <input type="checkbox" checked={item.check} className="pt-2 mr-2 w-4 h-4 rounded-md" onChange={e => handleCheck(index)}/>
+              <p className={item.check?"line-through decoration-2":""} onClick={e => handleCheck(index)}>{item.label}</p>
               <p className="mx-2">-</p>
-              <button className="px-2 rounded-md border-solid border-2 border-black hover:bg-red-400" onClick={e => handleRemover(index)}>deletar</button>
+              <button className="px-2 rounded-md border-solid border-2 border-black hover:bg-red-400" onClick={e => handleDeletar(index)}>deletar</button>
             </li>
           )}
         </ul>
-        <p className="mb-2 justify-self-center">Itens concluídos: {checkCounter} | Itens restantes: {listaTarefas.length-checkCounter}</p>
+        <p className="mb-2 justify-self-center">Itens no carrinho: {checkCounter} | Itens restantes: {listaCompras.length-checkCounter}</p>
       </div>
       <div className="border-solid border-1 border-slate-600 bg-slate-600 rounded-b-md p-3 flex space-x-2 items-center justify-center w-96">
-          <button className="p-2 rounded-md border-solid border-2 border-sky hover:bg-green-400" onClick={e=>handleConcluidos()}>Limpar concluídos</button>
-          <button className="p-2 rounded-md border-solid border-2 border-sky hover:bg-red-400" onClick={e=>handleLimpar()}>Limpar lista</button>
+          <button className="p-2 rounded-md border-solid border-2 border-sky hover:bg-green-400" onClick={e=>handleApagarCarrinho()}>Apagar carrinho</button>
+          <button className="p-2 rounded-md border-solid border-2 border-sky hover:bg-red-400" onClick={e=>handleApagarLista()}>Apagar lista</button>
       </div>
     </div>
   );
