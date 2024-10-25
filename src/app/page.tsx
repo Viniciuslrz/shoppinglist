73b59f630da71c216de19b54d4325062e3f03df8
Vinicius Lorenz <48999197+Viniciuslrz@users.noreply.github.com>
2024-10-25 07:23:50 -0300

    Hover -> Active

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,7 @@ const page = ()=>{
       <h3 className="text-3xl">Lista de Compras</h3>
       <div className="border-solid border-1 border-slate-600 bg-slate-600 rounded-t-md mt-2 p-3 w-96 flex">
           <input onKeyDown={(e)=>{if(e.key==="Enter"){handleAdicionar(item)}}} className="grow rounded-md p-2 text-black" type="text" placeholder="O que deseja comprar?" value={item} onChange={e => setItem(e.target.value)}/>
-          <button className="ml-2 p-2 rounded-md border-solid border-2 hover:bg-blue-400" onClick={e=>handleAdicionar(item)}>Adicionar</button>
+          <button className="ml-2 p-2 rounded-md border-solid border-2 active:bg-blue-400" onClick={e=>handleAdicionar(item)}>Adicionar</button>
       </div>
       <div className="w-96 grid bg-slate-300 text-black">
         <p className="mt-2 justify-self-center"> Itens na lista: {listaCompras.length}</p>
@@ -81,15 +81,15 @@ const page = ()=>{
               <input type="checkbox" checked={item.check} className="pt-2 mr-2 w-4 h-4 rounded-md" onChange={e => handleCheck(index)}/>
               <p className={item.check?"line-through decoration-2":""} onClick={e => handleCheck(index)}>{item.label}</p>
               <p className="mx-2">-</p>
-              <button className="px-2 rounded-md border-solid border-2 border-black hover:bg-red-400" onClick={e => handleDeletar(index)}>deletar</button>
+              <button className="px-2 rounded-md border-solid border-2 border-black active:bg-red-400" onClick={e => handleDeletar(index)}>deletar</button>
             </li>
           )}
         </ul>
         <p className="mb-2 justify-self-center">Itens no carrinho: {checkCounter} | Itens restantes: {listaCompras.length-checkCounter}</p>
       </div>
       <div className="border-solid border-1 border-slate-600 bg-slate-600 rounded-b-md p-3 flex space-x-2 items-center justify-center w-96">
-          <button className="p-2 rounded-md border-solid border-2 border-sky hover:bg-green-400" onClick={e=>handleApagarCarrinho()}>Apagar carrinho</button>
-          <button className="p-2 rounded-md border-solid border-2 border-sky hover:bg-red-400" onClick={e=>handleApagarLista()}>Apagar lista</button>
+          <button className="p-2 rounded-md border-solid border-2 border-sky active:bg-green-400" onClick={e=>handleApagarCarrinho()}>Apagar carrinho</button>
+          <button className="p-2 rounded-md border-solid border-2 border-sky active:bg-red-400" onClick={e=>handleApagarLista()}>Apagar lista</button>
       </div>
     </div>
   );
